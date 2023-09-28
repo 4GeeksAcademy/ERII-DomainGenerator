bde5c00b27f7454fdf822b5449edc1c81d4a73ad
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-09-28 18:14:18 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "a"];
+  let adj = ["great", "big", "beautiful", "creepy", "nasty"];
+  let noun = ["jogger", "raccoon", "car", "pizza", "burger", "thief"];
+  let dom = [".com", ".org", ".es", ".net", ".xyz"];
+
+  function generateDomain() {
+    let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+    let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+    let randomDom = dom[Math.floor(Math.random() * dom.length)];
+
+    return randomPronoun + randomAdj + randomNoun + randomDom;
+  }
+
+  let generatedDomainElement = document.getElementById("generatedDomain");
+
+  let generatedDomain = generateDomain();
+  generatedDomainElement.textContent = generatedDomain;
+
+  let generateButton = document.getElementById("generateButton");
+
+  function onGenerateButtonClick() {
+    let generatedDomain = generateDomain();
+    generatedDomainElement.textContent = generatedDomain;
+  }
+
+  generateButton.addEventListener("click", onGenerateButtonClick);
 };
